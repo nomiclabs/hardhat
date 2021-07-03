4c1de0e4da97abfbd605e2d370c1ab5b4aba7c88
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2021-07-03 11:43:24 -0600

    Add Advanced sample project
    -  [ ] Verify and demonstrate usage of hardhat-etherscan.
    -  [ ] Resolve (fix or ignore) outstanding lint errors.
    -  [ ] Add automated tests of project creation.

---
+++ packages/hardhat-core/sample-projects/advanced/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    mocha: true,
+    node: true,
+  },
+  extends: [
+    "standard",
+    "plugin:prettier/recommended",
+    "plugin:node/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+};
